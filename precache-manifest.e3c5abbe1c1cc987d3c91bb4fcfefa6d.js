f88c79b367c1f077569e03f429b42969fa2a61bd
hjy <5336772+huangtobe@user.noreply.gitee.com>
2022-02-07 10:34:48 +0800

    update

---
+++ precache-manifest.e3c5abbe1c1cc987d3c91bb4fcfefa6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b5ade1c2eb0060c760",
+    "url": "/money-website/css/app.75887c7c.css"
+  },
+  {
+    "revision": "915f4de98a865b607aaf9c46b7ad4658",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "f8b5ade1c2eb0060c760",
+    "url": "/money-website/js/app.8a9783eb.js"
+  },
+  {
+    "revision": "966fdf01409f37ad6149",
+    "url": "/money-website/js/chunk-vendors.c21dbb67.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
